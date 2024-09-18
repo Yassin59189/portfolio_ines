676be1eaf077deafdbd089c56576b069d99873be
Yassin59189 <yassin.59189@gmail.com>
2024-09-18 13:47:07 +0100

    detail pages

--- script.js
+++ script.js
@@ -1,40 +1,84 @@
-const filterButtons = document.querySelectorAll(".filters button")
-const cards = document.querySelectorAll(".box_container .box")
-const email = document.getElementById("emailBtn")
+const filterButtons = document.querySelectorAll(".filters button");
+const nextBtn = document.querySelector(".next"); // or getElementsByClassName("next")[0]
+const previousBtn = document.querySelector(".previous"); // or getElementsByClassName("previous")[0]
+const cards = document.querySelectorAll(".box_container .box");
+const emailBtn = document.getElementById("emailBtn");
+const sections = document.querySelectorAll(".projects_container .box_container");
 
 const filterCards = (e) => {
-    document.querySelector(".active_button").classList.remove("active_button")
-    e.target.classList.add("active_button")
+  document.querySelector(".active_button").classList.remove("active_button");
+  e.target.classList.add("active_button");
 
-    cards.forEach(card => {
-        card.classList.add("hide")
-        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
-            card.classList.remove("hide")
-        }
-    })
-}
-
-filterButtons.forEach(button => button.addEventListener("click", filterCards))
-
-function scrollToBottom () {
-    var height = document.body.scrollHeight
-    window.scroll(0, height)
-}
-
-window.addEventListener('scroll', checkScroll)
-
-function checkScroll(){
-    const scrollIcon = document.querySelector(".scroll_to_bottom")
-    var height = document.documentElement.scrollHeight
-    var current = window.scrollY+window.innerHeight
-    if (current >= height) {
-        scrollIcon.classList.add("hide")
+  cards.forEach((card) => {
+    card.classList.add("hide");
+    if (card.dataset.name === e.target.dataset.name) {
+      card.classList.remove("hide");
     }
-    else {
-        scrollIcon.classList.remove("hide")
+
+    if (e.target.dataset.name === "all") {
+      if (!card.classList.contains("hide1")) {
+        card.classList.remove("hide");
+        showSection(currentSection);
+      }
     }
+    if (e.target.dataset.name !== "all") {
+        sections.forEach((section, i) => {
+              section.classList.remove("hide");
+          })
+    }
+  });
+};
+
+let currentSection = 0;
+
+function showSection(index) {
+  sections.forEach((section, i) => {
+    section.classList.add("hide");
+    if (i === index) {
+      section.classList.remove("hide");
+    }
+  });
 }
 
-emailBtn.addEventListener("click", function() {
-    window.location.href = "mailto:ines.mlaouhi.pro@gmail.com"
-})
+
+
+nextBtn.addEventListener("click", () => {
+  if (currentSection < sections.length - 1) {
+    currentSection++;
+    showSection(currentSection);
+    console.log(currentSection)
+  }
+});
+
+previousBtn.addEventListener("click", () => {
+  if (currentSection > 0) {
+    currentSection--;
+    showSection(currentSection);
+  }
+});
+
+showSection(currentSection);
+
+filterButtons.forEach((button) => button.addEventListener("click", filterCards));
+
+function scrollToBottom() {
+  var height = document.body.scrollHeight;
+  window.scroll(0, height);
+}
+
+window.addEventListener("scroll", checkScroll);
+
+function checkScroll() {
+  const scrollIcon = document.querySelector(".scroll_to_bottom");
+  var height = document.documentElement.scrollHeight;
+  var current = window.scrollY + window.innerHeight;
+  if (current >= height) {
+    scrollIcon.classList.add("hide");
+  } else {
+    scrollIcon.classList.remove("hide");
+  }
+}
+
+emailBtn.addEventListener("click", function () {
+  window.location.href = "mailto:ines.mlaouhi.pro@gmail.com";
+});
