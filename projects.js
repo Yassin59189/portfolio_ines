de1831c0bdce5f2d98d429042df211ebc2b4c144
Yassin59189 <yassin.59189@gmail.com>
2024-09-27 17:35:08 +0100

    final 1

--- projects.js
+++ projects.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll(".box_container .box");
 const nextBtn = document.querySelector(".next");
 const previousBtn = document.querySelector(".previous");
 const sections = document.querySelectorAll(".projects_container .box_container");
+const nextBtn2 = document.querySelector(".Nextbtn");
 
 
 const filterCards = (e) => {
@@ -40,7 +41,7 @@ const filterCards = (e) => {
   };
 
 
-  let currentSection = 0;
+let currentSection = 0;
 
 function showSection(index) {
   sections.forEach((section, i) => {
@@ -57,6 +58,28 @@ nextBtn.addEventListener("click", () => {
   if (currentSection < sections.length - 1) {
     currentSection++;
     showSection(currentSection);
+    if(currentSection === 0) {
+      nextBtn.classList.add("hide")
+      previousBtn.classList.add("hide")
+      nextBtn2.classList.remove("hide")
+    } else {
+      nextBtn.classList.remove("hide")
+      previousBtn.classList.remove("hide")
+      nextBtn2.classList.add("hide")
+    }
+    console.log(currentSection)
+  }
+});
+
+nextBtn2.addEventListener("click", () => {
+  if (currentSection < sections.length - 1) {
+    currentSection++;
+    showSection(currentSection);
+    if(currentSection === 1) {
+      nextBtn.classList.remove("hide")
+      previousBtn.classList.remove("hide")
+      nextBtn2.style.display = 'none'
+    }
     console.log(currentSection)
   }
 });
@@ -65,6 +88,10 @@ previousBtn.addEventListener("click", () => {
   if (currentSection > 0) {
     currentSection--;
     showSection(currentSection);
+  }if(currentSection === 0){
+    nextBtn2.style.display = ''
+    nextBtn.classList.add("hide")
+    previousBtn.classList.add("hide")
   }
 });
 
@@ -72,6 +99,29 @@ showSection(currentSection);
 
 
 
+
+
+function scrollToBottom() {
+  var height = document.body.scrollHeight;
+  window.scroll(0, height);
+}
+
+window.addEventListener("scroll", checkScroll);
+
+function checkScroll() {
+  const scrollIcon = document.querySelector(".scroll_to_bottom");
+  var height = document.documentElement.scrollHeight;
+  var current = window.scrollY + window.innerHeight;
+  if (current >= height) {
+    scrollIcon.classList.add("hide");
+  } else {
+    scrollIcon.classList.remove("hide");
+  }
+}
+
+emailBtn.addEventListener("click", function () {
+  window.location.href = "mailto:ines.mlaouhi.pro@gmail.com";
+});
 
 
 
